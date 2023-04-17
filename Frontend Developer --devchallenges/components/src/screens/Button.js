897f76a0f7086e8ea78d58fbce4b7f6377e3445d
Himanshu Joshi <68810782+farzigalib@users.noreply.github.com>
2023-04-17 08:27:45 +0000

    Pending changes exported from your codespace

--- Frontend Developer --devchallenges/components/src/screens/Button.js
+++ Frontend Developer --devchallenges/components/src/screens/Button.js
@@ -1,9 +1,24 @@
 import React from "react";
+import CustomButton from "../components/CustomButton";
 
 const Button = () => {
   return (
     <main className="main-content-container">
       <header>Buttons</header>
+      <div className="content-section">
+        <div className="block block-1">
+          <CustomButton
+            marginLeft={false}
+            btnHeading={"<Button />"}
+            classNameWithoutHover={"default"}
+          />
+          <CustomButton
+            isHover={true}
+            btnHeading={"&:hover, &:focus"}
+            classNameWithHover={"default default-hover"}
+          />
+        </div>
+      </div>
     </main>
   );
 };
